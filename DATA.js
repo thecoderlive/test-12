252a17ec2b91e541f719f955ee0ff08bb7a16901
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-09-28 14:01:06 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "ee0fe73f-cd27-4972-b7dc-4f4c51d487d0",
+    "home_feed": [
+        {
+            "id": "4abf0c0a-515b-4e64-9239-26af28d33867",
+            "artist_name": "child",
+            "home_carousel": [
+                {
+                    "id": "12f7e76b-b096-4a6e-a314-23e909f1242d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/29.png"
+                },
+                {
+                    "id": "0f08efce-6c2a-4e2d-a43a-5c539a370c86",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/231.png"
+                },
+                {
+                    "id": "1088180e-ac07-413e-b26c-e009a4ff027c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/368.png"
+                },
+                {
+                    "id": "247153aa-a383-43b4-ab77-1be527f70e00",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/222.png"
+                },
+                {
+                    "id": "b7ef66fc-1977-4250-919a-8ad1da7c4330",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/256.png"
+                }
+            ],
+            "art_caption": "heart",
+            "art_description": "Crime mouth admit receive should."
+        },
+        {
+            "id": "2e7942b0-30da-4fd8-a511-faada6738a40",
+            "artist_name": "thousand",
+            "home_carousel": [
+                {
+                    "id": "05724f3e-20d9-4a2e-bfe4-956f19603d69",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/83.png"
+                },
+                {
+                    "id": "c19f72ad-c714-4549-891e-59c43987cf22",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/114.png"
+                },
+                {
+                    "id": "e6f9542f-eb83-490b-94fd-8320e80b596d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/213.png"
+                },
+                {
+                    "id": "07a18476-00f5-4930-aaad-de4c8325f49f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/297.png"
+                },
+                {
+                    "id": "93294fdd-1b5c-49d8-b77f-5f907713a348",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/185.png"
+                }
+            ],
+            "art_caption": "born",
+            "art_description": "Four recent a. Point painting himself image political everybody place."
+        },
+        {
+            "id": "49ab4a4a-92c3-4f6c-853e-8775fa92784c",
+            "artist_name": "nature",
+            "home_carousel": [
+                {
+                    "id": "5fd90724-be2a-438c-9ff1-b212b45bb827",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/32.png"
+                },
+                {
+                    "id": "e2b4e8d9-815e-4c6d-90c5-6a3f84bfc5ec",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/234.png"
+                },
+                {
+                    "id": "4aaede53-f935-4c86-a2b0-a23418d6e01c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/92.png"
+                },
+                {
+                    "id": "f6d3fb51-3186-40fa-ae92-d1010086aa47",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/323.png"
+                },
+                {
+                    "id": "4b82124c-f71a-464e-a19a-440c02040eba",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/365.png"
+                }
+            ],
+            "art_caption": "I",
+            "art_description": "Rest trip take young. Politics visit have evening kid. Everyone force thing star per community."
+        },
+        {
+            "id": "2547f16c-602a-4594-93ff-79642981de25",
+            "artist_name": "start",
+            "home_carousel": [
+                {
+                    "id": "aa445d77-adc9-40de-9e58-00868a1729bf",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/2.png"
+                },
+                {
+                    "id": "2ddd0206-592f-4546-893f-0534631d8653",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/213.png"
+                },
+                {
+                    "id": "32ee1ba5-e252-407b-bb19-91dc2c857873",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/93.png"
+                },
+                {
+                    "id": "181ecfbd-fcaa-4ba4-9a4e-199ee3e64744",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/101.png"
+                },
+                {
+                    "id": "2f039bc4-665d-4bb4-8f34-3683352f7256",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/206.png"
+                }
+            ],
+            "art_caption": "there",
+            "art_description": "Power type coach here some support think. Teacher thousand within open red baby goal activity."
+        },
+        {
+            "id": "7e26f00e-f1a0-4d73-83d5-f6083d772284",
+            "artist_name": "help",
+            "home_carousel": [
+                {
+                    "id": "714b5c54-03f1-4400-b4c0-e1fffc90b00f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/158.png"
+                },
+                {
+                    "id": "36943a68-00c0-4205-89d6-080024afefa2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/120.png"
+                },
+                {
+                    "id": "765efd10-5d68-48ff-94d2-0bb4379f50ae",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/37.png"
+                },
+                {
+                    "id": "6f0a43fe-c0fd-4162-841a-44b3d2f0d30b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/346.png"
+                },
+                {
+                    "id": "774336bd-731b-4631-9d49-58f576305d74",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/74.png"
+                }
+            ],
+            "art_caption": "last",
+            "art_description": "Conference federal model their prove. Her front international author hundred establish."
+        }
+    ]
+}
